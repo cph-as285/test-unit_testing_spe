1837178664e039fa2865554544a2f5b5e6bc8ff9
Alexander Steen <ale.steen@hotmail.com>
2017-02-26 17:40:46 +0100

    Added method and implemented test. Using build in method

---
+++ Calender/Calender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calender
+{
+    public class Calender
+    {
+        public static int GetNumDaysinMonth(int month, int year)
+        {
+            return DateTime.DaysInMonth(year, month);
+        }
+    }
+}
