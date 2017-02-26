e051c326187bc8443271fa0b13b86bf6c7a38b84
Alexander Steen <ale.steen@hotmail.com>
2017-02-26 17:59:05 +0100

    Added insurance tests and template for impl with enum

---
+++ Insurance/Deductible.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insurance
+{
+    public class Deductible
+    {
+        public static int CalculateReimburse(bool deductibleMet, TypeOfVisit typeOfVisit)
+        {
+            return 0;
+        }
+    }
+}
